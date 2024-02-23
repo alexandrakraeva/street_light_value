60efef1eab20738584988753ab51fce79e38142e
alexandrakraeva <66311877+alexandrakraeva@users.noreply.github.com>
2024-02-23 11:41:32 +0100

    +++

---
+++ server.js
@@ -0,0 +1,19 @@
+﻿const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+app.get('/updateLux', (req, res) => {
+    const lux = req.query.lux; // Get the lux value from the query string
+    console.log(`Lux value received: ${lux}`);
+    io.emit('lux', { value: lux }); // Emit the lux value to all connected WebSocket clients
+    res.sendStatus(200);
+});
+
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
